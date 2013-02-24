04168291ccf360db991b43c1034e1d21e39a729d
Rob Martin <rob@version2beta.com>
2013-02-23 23:41:01 -0600

    This might work, maybe

--- streets.py
+++ streets.py
@@ -1,14 +1,84 @@
-class Regs():
-  def __init__(self, reg):
-    pass
+import sys
+import simplejson as json
+from pprint import pprint
+
+class Blocks():
+  def __init__(self):
+    self.blocks = {}
+
+  def __len__(self):
+    return len(self.blocks)
+
+  def add(self, street):
+    new_block = Block(street)
+    key = "%s %s %s" % (new_block.direction, new_block.street, new_block.suffix)
+    try:
+      self.blocks[key].append(new_block)
+    except KeyError:
+      self.blocks[key] = [new_block]
+
+  def reg(self, reg):
+    r = reg.split(',')
+    lowest = int(r[0])
+    highest = int(r[1])
+    key = r[2].replace("'", '')
+    reg_text = r[3].replace("'", '')
+    reg_key = r[4].replace("'", '').strip()
+    try:
+      for block in self.blocks[key]:
+        if lowest in range(block.lowest, block.highest) and highest in range(block.lowest, block.highest):
+          block.regs[reg_key] = reg_text
+    except KeyError:
+      pass
+
+  def dump(self):
+    retval = []
+    for k in self.blocks:
+      for b in self.blocks[k]:
+        retval.append(b.to_dict())
+    return retval
 
 class Block():
+  @classmethod
+  def Parse_address_range(self, a):
+    a = [int(x) for x in a if int(x) > 0]
+    if len(a):
+      smallest = min(a)
+      largest = max(a)
+      return smallest, largest
+    else:
+      return 0, 0
+
   def __init__(self, street):
     f = street.split(';')
-    self.row_id = f[0]
-    self.street = f[2]
-    self.direction = f[3]
-    self.suffix = f[4]
-    self.lowest = min(f[5:8])
-    self.highest = max(f[5:8])
+    self.gid = f[0]
+    self.record_number = f[1]
+    self.street = f[2].replace('"', '')
+    self.direction = f[3].replace('"', '')
+    self.suffix = f[4].replace('"', '')
+    self.lowest, self.highest = self.Parse_address_range(f[5:9])
     self.regs = {}
+
+  def to_dict(self):
+    return dict(
+        gid = self.gid,
+        record_number = self.record_number,
+        direction = self.direction,
+        street = self.street,
+        suffix = self.suffix,
+        lowest = self.lowest,
+        highest = self.highest,
+        regs = self.regs
+        )
+
+def main():
+  all_blocks = Blocks()
+  for line in open(sys.argv[1], 'r').readlines():
+    all_blocks.add(line)
+  for line in open(sys.argv[2], 'r').readlines():
+    all_blocks.reg(line.replace('"', ''))
+  print json.dumps(all_blocks.dump(), sort_keys = True, indent = 4)
+
+if __name__ == '__main__':
+  main()
+
--- tests.py
+++ tests.py
@@ -1,10 +1,10 @@
-from streets import Block
+from streets import *
 from expecter import expect
 from pprint import pprint
 
-streets = [
+blocks = [
   '1;19139;"COUNTY LINE";"W";"RD";6801;7599;6800;7598;"0102000000020000000AD7A3F0EF564341F6285C8FC4EB1A4189416095E2514341D578E9A6D0EA1A41";"STREETS"',
-  '2;19140;"COUNTY LINE";"W";"RD";7601;8399;7600;8398;"01020000000200000089416095E2514341D578E9A6D0EA1A4108AC1C4AAC4C4341CDCCCCCCB5E91A41";"STREETS"',
+  '2;19140;"COUNTY LINE";"W";"RD";7601;8399;0;0;"01020000000200000089416095E2514341D578E9A6D0EA1A4108AC1C4AAC4C4341CDCCCCCCB5E91A41";"STREETS"',
   '3;19141;"COUNTY LINE";"W";"RD";8401;9099;8400;9098;"01020000000200000008AC1C4AAC4C4341CDCCCCCCB5E91A419CC420B08B47434117D9CEF767E81A41";"STREETS"',
   '4;19095;"COUNTY LINE";"W";"RD";9101;9499;9100;9498;"0102000000020000009CC420B08B47434117D9CEF767E81A4179E92601DA44434162105839A7E71A41";"STREETS"',
   '5;802834;"COUNTY LINE";"W";"RD";9501;9899;9500;9898;"01020000000200000079E92601DA44434162105839A7E71A41CBA145B65342434108AC1C5AF2E61A41";"STREETS"'
@@ -45,12 +45,26 @@ regs = [
   "9900,9999,'W COUNTY LINE RD','No Winter Night Parking',noWinter"
 ]
 
-def test_import_street():
-  blocks = []
-  for street in streets:
-    blocks.append(Block(street))
-  expect(blocks[0].lowest) >= 0
+all_blocks = Blocks()
+key = "W COUNTY LINE RD"
+for b in blocks:
+  all_blocks.add(b)
 
-def test_import_parking():
-  pass
+def test_parse_address_range():
+  expect(Block.Parse_address_range([0, 1, 1, 1])) == (1, 1)
+  expect(Block.Parse_address_range([0, 1, 1, 1])) == (1, 1)
 
+def test_import_a_street():
+  block = Block(blocks[0])
+  expect(block.lowest) == 6800
+
+def test_import_a_street_with_zero_addresses():
+  block = Block(blocks[1])
+  expect(block.lowest) == 7601
+
+def test_blocks():
+  expect(len(all_blocks)) == 1
+
+def test_import_regs():
+  all_blocks.reg(regs[0])
+  expect(len(all_blocks.blocks[key][0].regs)) == 1
