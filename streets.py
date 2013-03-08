0144cd9860953051a52c47703f5015bcec330920
Rob Martin <rob@version2beta.com>
2013-03-07 18:11:46 -0600

    Fixed reg import

--- streets.py
+++ streets.py
@@ -1,5 +1,7 @@
 import sys
+import re
 import simplejson as json
+import csv
 from pprint import pprint
 
 class Blocks():
@@ -12,21 +14,21 @@ class Blocks():
   def add(self, street):
     new_block = Block(street)
     key = "%s %s %s" % (new_block.direction, new_block.street, new_block.suffix)
+    key = re.sub('[^0-9a-zA-Z ]+', '', key)
     try:
       self.blocks[key].append(new_block)
     except KeyError:
       self.blocks[key] = [new_block]
 
-  def reg(self, reg):
-    r = reg.split(',')
+  def reg(self, r):
     lowest = int(r[0])
     highest = int(r[1])
-    key = r[2].replace("'", '')
+    key = re.sub('[^0-9a-zA-Z ]+', '', r[2])
     reg_text = r[3].replace("'", '')
     reg_key = r[4].replace("'", '').strip()
     try:
       for block in self.blocks[key]:
-        if lowest in range(block.lowest, block.highest) and highest in range(block.lowest, block.highest):
+        if block.lowest in range(lowest, highest) or block.highest in range(lowest, highest):
           block.regs[reg_key] = reg_text
     except KeyError:
       pass
@@ -49,8 +51,7 @@ class Block():
     else:
       return 0, 0
 
-  def __init__(self, street):
-    f = street.split(';')
+  def __init__(self, f):
     self.gid = f[0]
     self.record_number = f[1]
     self.street = f[2].replace('"', '')
@@ -73,10 +74,18 @@ class Block():
 
 def main():
   all_blocks = Blocks()
-  for line in open(sys.argv[1], 'r').readlines():
-    all_blocks.add(line)
-  for line in open(sys.argv[2], 'r').readlines():
-    all_blocks.reg(line.replace('"', ''))
+  with open(sys.argv[1]) as f:
+    dialect = csv.Sniffer().sniff(f.read(1024))
+    f.seek(0)
+    streets = csv.reader(f, dialect)
+    for street in streets:
+      all_blocks.add(street)
+  with open(sys.argv[2]) as f:
+    dialect = csv.Sniffer().sniff(f.read(1024))
+    f.seek(0)
+    regs = csv.reader(f, dialect)
+    for reg in regs:
+      all_blocks.reg(reg)
   print json.dumps(all_blocks.dump(), sort_keys = True, indent = 4)
 
 if __name__ == '__main__':
